c46d7f90795456d772d26b5cd672e769a2c4e444
kinsprite <qxqs11@126.com>
2020-06-26 14:31:01 +0800

    no translateX

--- src/home/HomeApp.tsx
+++ src/home/HomeApp.tsx
@@ -12,41 +12,31 @@ function HomeApp(): JSX.Element {
       <header className={styles.AppHeader}>
         <img src={logo} className={styles.AppLogo} alt="logo" />
         <p>
-          <code>React Micro Frontends @ 2020</code>
+          <code>Hello, React Micro Frontends @ 2020</code>
         </p>
-        <ul>
-          <li>
-            <Link to="/home" className={styles.AppLink}>Home</Link>
-          </li>
-          <li>
-            <Link to="/app-example" className={styles.AppLink}>App Example</Link>
-          </li>
-          <li>
-            <Link to="/app-example/sub" className={styles.AppLink}>{'App Example\'s Sub'}</Link>
-          </li>
-          <li>
-            <Link to="/sub-at-root" className={styles.AppLink}>Sub at Root</Link>
-          </li>
-        </ul>
-      </header>
-      <footer className={styles.AppFooter}>
         <a
-          className={styles.FooterLink}
+          className={styles.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
-        <a
-          className={styles.FooterLink}
-          href="https://qinzhiqiang.cn"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Micro Frontends
-        </a>
-      </footer>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/app-example">App Example</Link>
+          </li>
+          <li>
+            <Link to="/app-example/sub">{'App Example\'s Sub'}</Link>
+          </li>
+          <li>
+            <Link to="/sub-at-root">Sub at Root</Link>
+          </li>
+        </ul>
+      </header>
     </div>
   );
 }
